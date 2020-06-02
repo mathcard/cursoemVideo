80d314ece98aaca9e9efbd0b588262e81b8d80d3
mathcard <matheus.carmo@hotmail.com>
2020-06-01 23:05:30 -0300

    Estruturas de repetição

---
+++ javascript/while.js
@@ -0,0 +1,23 @@
+/*console.log('Vai começar!')
+for(c=1; c<=3; c++){
+  console.log(c);
+}
+console.log('Vai sair!');
+*/
+
+// Contar
+/*var num1 = 0;
+var num2 = 12;
+var salto = 1;
+while(num1 < num2){
+  console.log(num1);
+  num1 += salto;
+}*/
+
+// Taboada
+var num = 8;
+var res = '';
+for(x=0; x<=10; x++){
+  res = num * x;
+  console.log(`${num} x ${x} = ${res}`);  
+}
