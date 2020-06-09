e7dd9d4f3d7f3f4e1170b0584d4da9b2f0bae3c7
mathcard <matheus.carmo@hotmail.com>
2020-06-08 22:34:21 -0300

    Terminando aula de função

---
+++ javascript/aulaFunctions/funcao02.js
@@ -0,0 +1,6 @@
+// Considerar 0 para parametro vazio
+function soma(n1=0, n2=0){
+  return n1+n2;
+}
+
+console.log(soma(12, 2));
---
+++ javascript/aulaVetores/ambiente.js
@@ -0,0 +1,21 @@
+let num = [2, 4, 7, 3, 9];
+num[0] = 1;
+num.push(8);
+console.log(num);
+console.log(`Tamanho: ${num.length}`);
+console.log(num.sort());
+
+/*
+for(x=0; x<num.length; x++){
+  console.log(`Posição ${x}: ${num[x]}`);
+}*/
+
+for(let pos in num ){
+  console.log(`Posição ${pos}: ${num[pos]}`);
+}
+
+let find = num.indexOf(7); // Pesquisando se o valor existe no vetor e devolvendo a posição
+console.log(find)
+
+
+
